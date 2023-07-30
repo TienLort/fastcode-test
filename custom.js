138a4872799502edec0e59e5bc1c099cd4aa5a8e
TienLort <vantien281201@gmail.com>
2023-07-30 11:43:58 +0700

    day 2

--- custom.js
+++ custom.js
@@ -13,8 +13,46 @@ links.forEach((link) => {
     links.forEach((link) => {
       link.classList.remove("change-color");
     });
-
-    // Thêm lớp "change-color" cho thẻ <a> được click
     this.classList.add("change-color");
   });
+});
+
+const numberLinks = document.querySelectorAll(".number-link");
+numberLinks.forEach((link) => {
+  link.addEventListener("click", function (event) {
+    // event.preventDefault();
+    numberLinks.forEach((otherLink) => {
+      otherLink.classList.remove("active");
+    });
+    this.classList.add("active");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  function highlightCurrentSection() {
+    const sections = document.querySelectorAll("section");
+    const links = document.querySelectorAll(".navbar a");
+    const numberLinks = document.querySelectorAll(".number-link");
+
+    let currentSectionIndex = sections.length - 1;
+    for (let i = 0; i < sections.length; i++) {
+      const rect = sections[i].getBoundingClientRect();
+
+      if (rect.top <= 0 && rect.bottom >= 0) {
+        currentSectionIndex = i;
+        break;
+      }
+    }
+    numberLinks.forEach((otherLink) => {
+      otherLink.classList.remove("active");
+    });
+    numberLinks[currentSectionIndex].classList.add("active");
+    links.forEach((link) => link.classList.remove("change-color"));
+
+    links[currentSectionIndex + 1].classList.add("change-color");
+  }
+
+  highlightCurrentSection();
+
+  window.addEventListener("scroll", highlightCurrentSection);
 });
